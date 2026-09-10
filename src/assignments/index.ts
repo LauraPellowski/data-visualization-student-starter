0f8d5ac15af3ae8297bb936851dd0bf2efb6248d
unknown <lmpellowski@gmail.com>
2026-09-09 21:40:09 -0400

    First visualization and task analysis
    - Creates week 3 visualization representing MBTA ridership by line over
      time.
    - Adds "Task Analysis" section to final FINAL_PROJECT_EXPLORATION_1.

--- src/assignments/index.ts
+++ src/assignments/index.ts
@@ -1,6 +1,7 @@
 import type { ComponentType } from 'react';
 import { ResponsivePseudoScatterPlot } from './week-01/ResponsivePseudoScatterPlot';
 import { LoadAndSummarizeDataset } from './week-02/LoadAndSummarizeDataset';
+import { ScatterplotBasic } from './week-03/ScatterplotBasic';
 
 export interface Assignment {
   id: string;
@@ -18,6 +19,11 @@ export const assignments: Assignment[] = [
     id: '2',
     name: 'Week 2',
     component: LoadAndSummarizeDataset,
+  },
+  {
+    id: '3',
+    name: 'Week 3',
+    component: ScatterplotBasic,
   },
 ];
 
